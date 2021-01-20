02158fcb1e7f52c3d79ae0a28f1f4f72ec93f501
polhek <ziga.groselj@gmail.com>
2021-01-20 20:50:51 +0100

    F Commit

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const fs = require('fs');
 const app = express();
 
 // setting up static page...
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+// catch error if page doesn't exist...
 app.use(function (req, res) {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
+// port variable
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
